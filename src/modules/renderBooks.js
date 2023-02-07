eba609897aacd5561b03610824c1c60f02838b53
shasherazi <hassanrandomz@gmail.com>
2023-02-07 17:49:03 +0500

    Add like and comment button

--- src/modules/renderBooks.js
+++ src/modules/renderBooks.js
@@ -7,6 +7,13 @@ const renderBooks = (book) => {
         <img src="${book.formats['image/jpeg']}" alt="${book.title}">
         <h2 class="book-title">${book.title}</h2>
         <p class="book-author">${book.authors.length === 0 ? 'N/A' : book.authors[0].name}</p>
+        <div class="like-and-comment">
+            <button class="comment-btn">
+                Comment <i class="fa-solid fa-comments"></i>
+            </button>
+            <i class="fa-regular fa-heart empty"></i>
+            <i class="fa-solid fa-heart filled hidden"></i>
+        </div>
     `;
   bookSection.appendChild(bookDiv);
 };
