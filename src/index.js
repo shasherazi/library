4d29bfe1747af8f03e1845476d88a6cb1f8c2cc3
shasherazi <hassanrandomz@gmail.com>
2023-02-07 13:08:51 +0500

    Remove bootstrap

--- src/index.js
+++ src/index.js
@@ -1,9 +1,1 @@
 import './style.css';
-import '../assets/bootstrap-5.3.0-alpha1-dist/css/bootstrap.min.css';
-import '../assets/bootstrap-5.3.0-alpha1-dist/js/bootstrap.bundle.js';
-import Logo from '../assets/Logo.jpg';
-
-const brand = document.querySelector('.navbar-brand');
-const myLogo = new Image();
-myLogo.src = Logo;
-brand.appendChild(myLogo);
