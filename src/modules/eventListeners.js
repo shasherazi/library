0a6ad19bc2ea8cc50032b6a8963a433a63943a2a
shasherazi <hassanrandomz@gmail.com>
2023-02-07 21:52:15 +0500

    Add new like by clicking the like button

--- src/modules/eventListeners.js
+++ src/modules/eventListeners.js
@@ -1,11 +1,24 @@
 import getBooks from './getBooks.js';
+import { addLike } from './involvementAPI/getLikes.js';
 import renderLikes from './involvementAPI/renderLikes.js';
 import renderBooks from './renderBooks.js';
+// import { addLike } from './involvementAPI/getLikes.js';
+
+const booksSection = document.querySelector('.books');
 
 document.addEventListener('DOMContentLoaded', async () => {
   const books = await getBooks();
   books.results.forEach((book) => {
     renderBooks(book);
-    renderLikes();
   });
+  renderLikes();
+});
+
+booksSection.addEventListener('click', (e) => {
+  if (e.target.closest('.like-btn')) {
+    const bookId = e.target.closest('.book-div').querySelector('.book-id').textContent;
+    console.log(bookId);
+    addLike(bookId);
+    renderLikes();
+  }
 });
--- src/modules/involvementAPI/renderLikes.js
+++ src/modules/involvementAPI/renderLikes.js
@@ -1,13 +1,14 @@
-import getLikes from './getLikes.js';
+import { getLikes } from './getLikes.js';
 
 const renderLikes = async () => {
   const likes = await getLikes();
   const likesContainer = document.querySelectorAll('.likes');
   likesContainer.forEach((like) => {
-    const bookId = like.parentElement.parentElement.firstElementChild.textContent;
-    const bookLikes = likes.filter((item) => item.item_id === parseInt(bookId, 10));
+    const bookId = like.parentElement.parentElement.parentElement.firstElementChild.textContent;
+    const bookLikes = likes.filter((item) => parseInt(item.item_id, 10) === parseInt(bookId, 10));
     like.textContent = bookLikes.length === 0 ? 0 : bookLikes[0].likes;
   });
+  console.log(likes);
 };
 
 export default renderLikes;
